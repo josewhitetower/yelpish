e60456c57d4b37e896b008fb41a85c79803ef332
Jose Torreblanca <josewhitetower@gmail.com>
2021-08-10 02:37:32 +0200

    added prehook 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@
 import {
   computed, defineComponent, ref, watchEffect,
 } from 'vue';
-import { getBusinesses } from '@/api/index';
 import { Business } from '@/types/index';
+import { getBusinesses } from '@/api/index';
 
 export default defineComponent({
   name: 'Home',
