d50fd3a733403336f5ee23cc0bed63120aeadfff
Jose Torreblanca <josewhitetower@gmail.com>
2021-08-12 00:35:05 +0200

    added useBusiness composable

--- src/views/Business.vue
+++ src/views/Business.vue
@@ -11,40 +11,21 @@
 </template>
 <script lang="ts">
 import {
-  computed, defineComponent, ref, watchEffect,
+  defineComponent, ref, watchEffect,
 } from 'vue';
-import { useRoute } from 'vue-router';
 import { IBusiness } from '@/types/index';
 import GoogleMap from '@/components/GoogleMap.vue';
-import { getBusiness } from '../api/index';
+import useBusiness from '../composables/useBusiness';
 
 export default defineComponent({
   name: 'Business',
   components: { GoogleMap },
 
   setup() {
-    const route = useRoute();
     const business = ref<IBusiness | null>(null);
-    const query = computed(() => `
-    {
-        business(id: "${route.params.alias}"){
-            name,
-            alias,
-            url,
-            rating,
-            photos,
-            hours {
-                is_open_now
-                open { start, end }
-            },
-            reviews { rating, user { name }, time_created },
-            coordinates { latitude, longitude },
-            location { formatted_address }
-        }
-    }`);
 
     watchEffect(async () => {
-      business.value = await getBusiness(query.value);
+      business.value = await useBusiness();
     });
 
     return { business };
