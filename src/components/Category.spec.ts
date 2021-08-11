2483f26757c1dc1bad653033a61f841dda6b70f5
Jose Torreblanca <josewhitetower@gmail.com>
2021-08-11 22:25:07 +0200

    added vue-test-utils

---
+++ src/components/Category.spec.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { mount } from '@vue/test-utils';
+import { ICategory } from '../types/index';
+import Category from '@/components/Category.vue';
+
+const category: ICategory = {
+  alias: 'restaurants',
+  icon: 'restaurant',
+  color: '#6366F1 ',
+};
+
+describe('Category.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(Category, {
+      props: {
+        isSelected: false,
+        category,
+      },
+    });
+    expect(wrapper.html().includes(category.color));
+    expect(wrapper.text()).toContain(category.icon);
+  });
+});
