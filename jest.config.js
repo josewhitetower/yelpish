399a01bdbda472d06cb84f71d967a178da6bcabc
Jose Torreblanca <josewhitetower@gmail.com>
2021-08-11 21:41:05 +0100

    Feat/added test (#10)
    * changed padding and header layout
    
    * added vue-test-utils
    
    * added unit test pipeline
    
    * increased coverage threshold
    
    * Revert "increased coverage threshold"
    
    This reverts commit 71c56224b66fbb7fb8b3bb21780d95c4a9cedd41.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.vue', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      statements: 4,
+    },
+  },
+};
